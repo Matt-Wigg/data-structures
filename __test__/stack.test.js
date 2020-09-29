0acbf43d3b5852b3c12441a53ba007df67732a46
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-09-28 19:14:28 -0700

    Cleans Stack tests and comments

--- __test__/stack.test.js
+++ __test__/stack.test.js
@@ -10,11 +10,9 @@ describe('Stack', () => {
   });
 
   describe('Stack properties and methods:', () => {
-
     test('Stack.storage property exists', () => {
       expect(stack.storage).toBeDefined();
     });
-
     test('Stack.storage is an Array', () => {
       expect(Array.isArray(stack.storage)).toBe(true);
     });
@@ -26,11 +24,9 @@ describe('Stack', () => {
         expect(typeof stack[method]).toBe('function');
       });
     });
-
   });
 
   describe('Stack.push()', () => {
-
     test('Stack.push() should add items to the top of the Stack', () => {
       stack.push('Hello');
       expect(stack.pop()).toEqual('Hello');
@@ -41,13 +37,9 @@ describe('Stack', () => {
       stack.push('');
       expect(stack.pop()).toEqual('');
     });
-
   });
 
-  const bar = () => ({ baz: 5, boz: 1 })
-
   describe('Stack.peek()', () => {
-
     test('Stack.peek() should return the top item from the Stack', () => {
       stack.push(4);
       expect(stack.peek()).toEqual(4);
@@ -58,7 +50,6 @@ describe('Stack', () => {
       stack.pop();
       expect(stack.peek()).toEqual(4);
     });
-
     test('Stack.peek() should not delete the returned item from the Stack', () => {
       stack.push([1, 2, 3]);
       stack.peek();
@@ -68,11 +59,9 @@ describe('Stack', () => {
       expect(stack.peek()).toEqual('Stack');
       expect(stack.peek()).toEqual('Stack');
     });
-
   });
 
   describe('Stack.push()', () => {
-
     test('Stack.push() should add items to the top of the Stack', () => {
       stack.push('Hello');
       expect(stack.pop()).toEqual('Hello');
@@ -83,11 +72,9 @@ describe('Stack', () => {
       stack.push('');
       expect(stack.pop()).toEqual('');
     });
-
   });
 
   describe('Stack.pop()', () => {
-
     test('Stack.pop() should return the top item from the Stack', () => {
       stack.push('Hello');
       expect(stack.pop()).toEqual('Hello');
@@ -98,7 +85,6 @@ describe('Stack', () => {
       stack.push('');
       expect(stack.pop()).toEqual('');
     });
-
     test('Stack.pop() should delete the top item from the Stack', () => {
       stack.push('Hello');
       expect(stack.pop()).toEqual('Hello');
@@ -109,20 +95,16 @@ describe('Stack', () => {
       expect(stack.pop()).toEqual(1337);
       expect(stack.pop()).toEqual(undefined);
     });
-
   });
 
   describe('Stack.isEmpty()', () => {
-
     test('Stack.isEmpty() should return true when the Stack is empty', () => {
       expect(stack.isEmpty()).toEqual(true);
     });
-
     test('Stack.isEmpty() should return false when the Stack is not empty', () => {
       stack.push('Hello');
       expect(stack.isEmpty()).toEqual(false);
     });
-
     test('Stack.isEmpty() should change from true to false when items are added', () => {
       expect(stack.isEmpty()).toEqual(true);
       stack.push('Hello');
@@ -132,39 +114,33 @@ describe('Stack', () => {
       stack.push('');
       expect(stack.isEmpty()).toEqual(false);
     });
-
   });
 
   describe('Stack.printStack()', () => {
+    const testFunction = () => null;
+    const testObject = {hello: 'world!', theUniverse: 42, aFunction: testFunction};
 
     test('Stack.printStack() should return an empty string when the Stack is empty', () => {
       expect(stack.printStack()).toEqual('');
     });
-
     test('Stack.printStack() should return the contents of the Stack as a string', () => {
       stack.push(4);
       stack.push(5);
       expect(stack.printStack()).toEqual('4 5');
       expect(typeof stack.printStack()).toEqual('string');
     });
-
-    const testFunction = () => null;
-    const testObject = {hello: 'world!', theUniverse: 42, aFunction: testFunction};
     test('Stack.printStack() should return objects and functions', () => {
       stack.push(testFunction);
       expect(stack.printStack()).toEqual('() => null');
       stack.push(testObject);
       expect(stack.printStack()).toEqual('() => null [object Object]');
     });
-
   });
 
   describe('Stack.size()', () => {
-
     test('Stack.size() should return 0 when the Stack is empty', () => {
       expect(stack.size()).toEqual(0);
     });
-
     test('Stack.size() should return the correct size when items are added to the Stack', () => {
       expect(stack.size()).toEqual(0);
       stack.push(4);
@@ -174,7 +150,6 @@ describe('Stack', () => {
       stack.push(5);
       expect(stack.size()).toEqual(4);
     });
-
     test('Stack.size() should return 0 if pop() is called on an empty Stack', () => {
       expect(stack.size()).toEqual(0);
       stack.push(4);
@@ -185,7 +160,6 @@ describe('Stack', () => {
       stack.pop();
       expect(stack.size()).toEqual(0);
     });
-
   });
 
 });
--- stack/stack.js
+++ stack/stack.js
@@ -1,38 +1,35 @@
-// a stack holds a list of elements
-// last in, first out
-
 class Stack {
   constructor() {
     this.storage = [];
   }
 
-  // O(1) Constant
-  push(value) { // adds an item to the top of the stack.
+  // adds an item to the top of the stack.
+  push(value) {
     this.storage.push(value);
   }
 
-  // O(1) Constant
-  peek() { // returns the top value without deleting.
+  // returns the top item from the stack.
+  peek() {
     return this.storage[this.size() - 1];
   }
 
-  // O(1) Constant
-  pop() { // removes the top item from the stack.
+  // removes and returns the top item from the stack.
+  pop() {
     return this.storage.pop();
   }
 
-  // O(1) Constant
-  isEmpty() { // checks if the stack is empty.
+  // checks if the stack is empty.
+  isEmpty() {
     return this.size() === 0;
   }
 
-  // O(n) Linear
-  printStack() { // prints the current stack.
+  // prints the contents of the stack.
+  printStack() {
     return this.storage.join(' ');
   }
 
-  // O(1) Constant
-  size() { // returns how many items are in the stack.
+  // returns how many items are in the stack.
+  size() {
     return this.storage.length;
   }
 }
