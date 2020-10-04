472023a062abef62a2f05048edc2c07e6c9300e5
Matt-Wigg <m.p.wigglesworth@gmail.com>
2020-10-04 14:14:13 -0700

    Adds Set shell

--- set/set.js
+++ set/set.js
@@ -1,1 +1,20 @@
-// TODO: Implement Set
+class Set {
+  constructor() {
+    this.storage = {};
+  }
+
+  add(value) {
+    this.storage[value] = 1;
+  }
+
+  contains(value) {
+    if (this.storage[value]) return true;
+    return false;
+  }
+
+  remove(value) {
+    delete this.storage[value];
+  }
+}
+
+module.exports = Set;
